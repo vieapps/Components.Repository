3d1771c115899f86713cba38bcb521f05568ff88
Quynh Nguyen <mr.quynh@gmail.com>
2017-07-09 00:09:23 +0700

    Add features of run-time objects

---
+++ DataStructs.cs
@@ -0,0 +1,191 @@
+﻿#region Related components
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json.Linq;
+#endregion
+
+namespace net.vieapps.Components.Repository
+{
+	/// <summary>
+	/// Information of a data source
+	/// </summary>
+	[Serializable, DebuggerDisplay("Name = {Name}, Mode = {Mode}")]
+	public class DataSource
+	{
+		public DataSource() { }
+
+		#region Properties
+		/// <summary>
+		/// Gets the name of the data source
+		/// </summary>
+		public string Name { get; internal set; }
+
+		/// <summary>
+		/// Gets the working mode
+		/// </summary>
+		public Repository.RepositoryMode Mode { get; internal set; }
+
+		/// <summary>
+		/// Gets the name of the connection string (for working with database server)
+		/// </summary>
+		public string ConnectionStringName { get; internal set; }
+
+		/// <summary>
+		/// Gets the name of the database (for working with database server)
+		/// </summary>
+		public string DatabaseName { get; internal set; }
+		#endregion
+
+		#region Parse
+		internal static DataSource FromJson(JObject settings)
+		{
+			if (settings == null)
+				throw new ArgumentNullException("settings");
+			else if (settings["name"] == null)
+				throw new ArgumentNullException("name", "[name] attribute of settings");
+			else if (settings["mode"] == null)
+				throw new ArgumentNullException("mode", "[mode] attribute of settings");
+
+			// initialize
+			var dataSource = new DataSource()
+			{
+				Name = (settings["name"] as JValue).Value as string,
+				Mode = (RepositoryMode)Enum.Parse(typeof(RepositoryMode), (settings["mode"] as JValue).Value as string)
+			};
+
+			// name of connection string (SQL and NoSQL)
+			if (dataSource.Mode.Equals(RepositoryMode.SQL) || dataSource.Mode.Equals(RepositoryMode.NoSQL))
+			{
+				if (settings["connectionStringName"] == null)
+					throw new ArgumentNullException("connectionStringName", "[connectionStringName] attribute of settings");
+				dataSource.ConnectionStringName = (settings["connectionStringName"] as JValue).Value as string;
+			}
+
+			// name of database (NoSQL)
+			if (dataSource.Mode.Equals(RepositoryMode.NoSQL))
+			{
+				if (settings["databaseName"] == null)
+					throw new ArgumentNullException("databaseName", "[databaseName] attribute of settings");
+				dataSource.DatabaseName = (settings["databaseName"] as JValue).Value as string;
+			}
+
+			return dataSource;
+		}
+		#endregion
+
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Access permissions of a resource (means working permissions of a run-time entity)
+	/// </summary>
+	[Serializable]
+	public class AccessPermissions
+	{
+
+		public AccessPermissions()
+		{
+			this.DownloadableRoles = new HashSet<string>();
+			this.DownloadableUsers = new HashSet<string>();
+			this.ViewableRoles = new HashSet<string>();
+			this.ViewableUsers = new HashSet<string>();
+			this.ContributiveRoles = new HashSet<string>();
+			this.ContributiveUsers = new HashSet<string>();
+			this.EditableRoles = new HashSet<string>();
+			this.EditableUsers = new HashSet<string>();
+			this.ModerateRoles = new HashSet<string>();
+			this.ModerateUsers = new HashSet<string>();
+			this.AdministrativeRoles = new HashSet<string>();
+			this.AdministrativeUsers = new HashSet<string>();
+		}
+
+		#region Properties
+		/// <summary>
+		/// Gets or sets the collection of identity of working roles that able to download files/attachments of the published resources
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> DownloadableRoles { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of users that able to download files/attachments of the published resources
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> DownloadableUsers { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of working roles that able to view the details (means read-only on published resources)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> ViewableRoles { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of users that able to view the details (means read-only on published resources)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> ViewableUsers { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of working roles that able to contribute (means create new and view the published/their own resources)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> ContributiveRoles { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of users that able to contribute (means create new and view the published/their own resources)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> ContributiveUsers { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of working roles that able to edit (means create new and re-update the published resources)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> EditableRoles { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of users that able to edit (means create new and re-update the published resources)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> EditableUsers { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of working roles that able to moderate (means moderate all kinds of resources)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> ModerateRoles { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of users that able to moderate (means moderate all kinds of resources)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> ModerateUsers { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of working roles that able to manage (means full access)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> AdministrativeRoles { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of identity of users that able to manage (means full access)
+		/// </summary>
+		[BsonIgnoreIfNull]
+		public HashSet<string> AdministrativeUsers { get; set; }
+		#endregion
+
+		internal static bool IsEmpty(HashSet<string> roles, HashSet<string> users)
+		{
+			return (roles == null || roles.Count < 1) && (users == null || users.Count < 1);
+		}
+
+		internal static bool IsNotEmpty(HashSet<string> roles, HashSet<string> users)
+		{
+			return (roles != null && roles.Count > 0) || (users != null && users.Count > 0);
+		}
+
+	}
+}
---
+++ Interfaces.cs
@@ -0,0 +1,408 @@
+﻿#region Related components
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+using net.vieapps.Components.Utility;
+#endregion
+
+namespace net.vieapps.Components.Repository
+{
+	/// <summary>
+	/// Repository (means information of a business module object in run-time)
+	/// </summary>
+	public interface IRepository
+	{
+
+		#region Properites
+		/// <summary>
+		/// Gets the identity (primary-key)
+		/// </summary>
+		string ID { get; }
+
+		/// <summary>
+		/// Gets the title
+		/// </summary>
+		string Title { get; }
+
+		/// <summary>
+		/// Gets the identity of a business system that the object is belong to
+		/// </summary>
+		string SystemID { get; }
+		#endregion
+
+		#region Methods
+		#endregion
+
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Repository entity (means information of a business content-type object in run-time)
+	/// </summary>
+	public interface IRepositoryEntity
+	{
+
+		#region Properites
+		/// <summary>
+		/// Gets the identity (primary-key)
+		/// </summary>
+		string ID { get; }
+
+		/// <summary>
+		/// Gets the title
+		/// </summary>
+		string Title { get; }
+
+		/// <summary>
+		/// Gets the identity of a business system that the object is belong to
+		/// </summary>
+		string SystemID { get; }
+
+		/// <summary>
+		/// Gets the identity of a business repository that the object is belong to
+		/// </summary>
+		string RepositoryID { get; }
+
+		/// <summary>
+		/// Gets the definition of custom properties
+		/// </summary>
+		List<ExtendedPropertyDefinition> ExtendedPropertyDefinitions { get; }
+
+		/// <summary>
+		/// Gets the definition for working with custom properties on UI
+		/// </summary>
+		ExtendedUIDefinition ExtendedUIDefinition { get; }
+		#endregion
+
+		#region Methods
+		#endregion
+
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Business entity (means information of a business object in run-time)
+	/// </summary>
+	public interface IBusinessEntity
+	{
+
+		#region Properites
+		/// <summary>
+		/// Gets the identity (primary-key)
+		/// </summary>
+		string ID { get; }
+
+		/// <summary>
+		/// Gets the title
+		/// </summary>
+		string Title { get; }
+
+		/// <summary>
+		/// Gets the identity of a system (at run-time) that the object is belong to
+		/// </summary>
+		string SystemID { get; }
+
+		/// <summary>
+		/// Gets the identity of a business repository (at run-time) that the object is belong to
+		/// </summary>
+		string RepositoryID { get; }
+
+		/// <summary>
+		/// Gets the identity of a business entity (at run-time) that the object is belong to
+		/// </summary>
+		string EntityID { get; }
+
+		/// <summary>
+		/// Gets or sets the collection of extended properties
+		/// </summary>
+		Dictionary<string, object> ExtendedProperties { get; set; }
+
+		/// <summary>
+		/// Gets the business entity that marks as parent of this object
+		/// </summary>
+		IBusinessEntity Parent { get; }
+
+		/// <summary>
+		/// Gets or sets the original working permissions of this object
+		/// </summary>
+		AccessPermissions OriginalPermissions { get; set; }
+
+		/// <summary>
+		/// Gets the actual working permissions (mean the combined permissions)
+		/// </summary>
+		AccessPermissions WorkingPermissions { get; }
+		#endregion
+
+		#region Methods
+		#endregion
+
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Define a filtering expression
+	/// </summary>
+	public interface IFilterBy<T> where T : class
+	{
+		/// <summary>
+		/// Parses from JSON
+		/// </summary>
+		/// <param name="json"></param>
+		void Parse(JObject json);
+
+		/// <summary>
+		/// Converts to JSON
+		/// </summary>
+		/// <returns></returns>
+		JObject ToJson();
+
+		/// <summary>
+		/// Gets the statement of SQL
+		/// </summary>
+		/// <returns></returns>
+		Tuple<string, Dictionary<string, object>> GetSqlStatement();
+
+		/// <summary>
+		/// Gets the statement of No SQL
+		/// </summary>
+		/// <returns></returns>
+		FilterDefinition<T> GetNoSqlStatement();
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Called before creating new instance of object
+	/// </summary>
+	public interface IPreCreateHandler
+	{
+		/// <summary>
+		/// Fires before creating new instance of object (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by synchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object to create new instance</param>
+		/// <returns>true if the operation should be vetoed (means the operation will be cancelled when return value is true)</returns>
+		bool OnPreCreate(RepositoryContext context, object @object);
+
+		/// <summary>
+		/// Fires before creating new instance of object (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by asynchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object to create new instance</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <returns>true if the operation should be vetoed (means the operation will be cancelled when return value is true)</returns>
+		Task<bool> OnPreCreateAsync(RepositoryContext context, object @object, CancellationToken cancellationToken);
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Called after creating new instance of object successful
+	/// </summary>
+	public interface IPostCreateHandler
+	{
+		/// <summary>
+		/// Fires after creating new instance of object successful (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by synchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object that instance is created successful</param>
+		/// <remarks>
+		/// This method will be called in a seperated task thread for the best performance
+		/// </remarks>
+		void OnPostCreate(RepositoryContext context, object @object);
+
+		/// <summary>
+		/// Fires after creating new instance of object successful (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by asynchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object that instance is created successful</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <remarks>
+		/// This method will be called in a seperated task thread for the best performance
+		/// </remarks>
+		/// <returns></returns>
+		Task OnPostCreateAsync(RepositoryContext context, object @object, CancellationToken cancellationToken);
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Called before getting (loading) instance of object
+	/// </summary>
+	public interface IPreGetHandler
+	{
+		/// <summary>
+		/// Fires before getting instance of object (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by synchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="id">The string that presents object's identity to get instance</param>
+		/// <returns>true if the operation should be vetoed (means the operation will be cancelled when return value is true)</returns>
+		bool OnPreGet(RepositoryContext context, string id);
+
+		/// <summary>
+		/// Fires before getting instance of object (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by asynchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="id">The string that presents object's identity to get instance</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <returns>true if the operation should be vetoed (means the operation will be cancelled when return value is true)</returns>
+		Task<bool> OnPreGetAsync(RepositoryContext context, string id, CancellationToken cancellationToken);
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Called after getting instance of object successful
+	/// </summary>
+	public interface IPostGetHandler
+	{
+		/// <summary>
+		/// Fires after getting instance of object successful (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by synchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object that instance is getted successful</param>
+		/// <remarks>
+		/// This method will be called in a seperated task thread for the best performance
+		/// </remarks>
+		void OnPostGet(RepositoryContext context, object @object);
+
+		/// <summary>
+		/// Fires after getting instance of object successful (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by asynchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object that instance is getted successful</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <remarks>
+		/// This method will be called in a seperated task thread for the best performance
+		/// </remarks>
+		/// <returns></returns>
+		Task OnPostGetAsync(RepositoryContext context, object @object, CancellationToken cancellationToken);
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Called before updating instance of object
+	/// </summary>
+	public interface IPreUpdateHandler
+	{
+		/// <summary>
+		/// Fires before updating instance of object (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by synchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object to update instance</param>
+		/// <returns>true if the operation should be vetoed (means the operation will be cancelled when return value is true)</returns>
+		bool OnPreUpdate(RepositoryContext context, object @object);
+
+		/// <summary>
+		/// Fires before updating instance of object (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by asynchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object to update instance</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <returns>true if the operation should be vetoed (means the operation will be cancelled when return value is true)</returns>
+		Task<bool> OnPreUpdateAsync(RepositoryContext context, object @object, CancellationToken cancellationToken);
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Called after updating instance of object successful
+	/// </summary>
+	public interface IPostUpdateHandler
+	{
+		/// <summary>
+		/// Fires after updating instance of object successful (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by synchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object that instance is getted successful</param>
+		/// <remarks>
+		/// This method will be called in a seperated task thread for the best performance
+		/// </remarks>
+		void OnPostUpdate(RepositoryContext context, object @object);
+
+		/// <summary>
+		/// Fires after updating instance of object successful (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by asynchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object that instance is getted successful</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <remarks>
+		/// This method will be called in a seperated task thread for the best performance
+		/// </remarks>
+		/// <returns></returns>
+		Task OnPostUpdateAsync(RepositoryContext context, object @object, CancellationToken cancellationToken);
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Called before deleting instance of object
+	/// </summary>
+	public interface IPreDeleteHandler
+	{
+		/// <summary>
+		/// Fires before delete instance of object (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by synchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object to delete instance</param>
+		/// <returns>true if the operation should be vetoed (means the operation will be cancelled when return value is true)</returns>
+		bool OnPreDelete(RepositoryContext context, object @object);
+
+		/// <summary>
+		/// Fires before deleting instance of object (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by asynchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object to delete instance</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <returns>true if the operation should be vetoed (means the operation will be cancelled when return value is true)</returns>
+		Task<bool> OnPreDeleteAsync(RepositoryContext context, object @object, CancellationToken cancellationToken);
+	}
+
+	//  --------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Called after deleting instance of object successful
+	/// </summary>
+	public interface IPostDeleteHandler
+	{
+		/// <summary>
+		/// Fires after deleting instance of object successful (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by synchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object that instance is deleted successful</param>
+		/// <remarks>
+		/// This method will be called in a seperated task thread for the best performance
+		/// </remarks>
+		void OnPostDelete(RepositoryContext context, object @object);
+
+		/// <summary>
+		/// Fires after deleting instance of object successful (called by <see cref="RepositoryMediator">RepositoryMediator</see> when process by asynchronous way)
+		/// </summary>
+		/// <param name="context">Repository context that hold the transaction and state data</param>
+		/// <param name="object">The object that instance is deleted successful</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <remarks>
+		/// This method will be called in a seperated task thread for the best performance
+		/// </remarks>
+		/// <returns></returns>
+		Task OnPostDeleteAsync(RepositoryContext context, object @object, CancellationToken cancellationToken);
+	}
+
+}
